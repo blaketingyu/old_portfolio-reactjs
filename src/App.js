1306ff1cb284944eac9a01d5fa5e25a8b9c86668
blaketingyu <blaketantingyu@gmail.com>
2022-01-11 14:13:07 +0800

    navbar error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import About from './components/about/About';
+
 import Mainpage from './components/Mainpage';
-import Work from './components/work/Work';
+
 
 function App() {
   return (
--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Home from './home/Home';
 import About from './about/About';
 import Work from './work/Work';
-import Navbar from './Navbar';
 import Education from './education/Education';
 import Contact from './contact/Contact';
 
